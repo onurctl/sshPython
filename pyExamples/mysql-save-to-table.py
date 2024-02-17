5b2306c6b5c44be3a4f6bb73a032615bd7c4f29a
onurctl <77632313+onurctl@users.noreply.github.com>
2024-02-17 18:48:45 +0300

    Update mysql-save-to-table.py

--- pyExamples/mysql-save-to-table.py
+++ pyExamples/mysql-save-to-table.py
@@ -3,9 +3,9 @@ from mysql.connector import Error
 
 try:
     connection = mysql.connector.connect(host='localhost',
-                                         database='sakila',
-                                         user='root',
-                                         password='root')
+                                         database='',
+                                         user='',
+                                         password='')
 
     mySql_insert_query = """INSERT INTO data (intf, ipaddr, status, proto) 
                            VALUES 
@@ -24,6 +24,3 @@ finally:
     if connection.is_connected():
         connection.close()
         print("MySQL bağlantısı sonlandırıldı")
-
-# output u list e atıp oradan da tek tek elemanalrı dict1 = list[0], dict2 = list[1] alıp db sütunlarına
-# veya json olarak al oradan bu şekilde alıp dict e sonra aktar db ye
