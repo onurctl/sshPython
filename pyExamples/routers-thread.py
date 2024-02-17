9499118ed1e5d7fcafe9cd9cf9635280c5cb754f
onurctl <77632313+onurctl@users.noreply.github.com>
2024-02-17 19:11:46 +0300

    Update routers-thread.py

--- pyExamples/routers-thread.py
+++ pyExamples/routers-thread.py
@@ -10,23 +10,25 @@ def calistir(*ios):
 
 #t1 = threading.Thread(target=calistir, args = ("thread-1", ))
 #t2 = threading.Thread(target=calistir, args = ("thread-2", ))
+
 """
 ios1 = { 
     'device_type': 'cisco_ios',
-    'ip':   '192.168.50.2',
-    'username': 'admin',
-    'password': 'cisco',}
+    'ip':   '192.168..',
+    'username': '',
+    'password': '',}
 ios2 = {
     'device_type': 'cisco_ios',
-    'ip':   '192.168.50.3',
-    'username': 'admin2',
-    'password': 'cisco3',
+    'ip':   '192.168..',
+    'username': '2',
+    'password': '3',
 }
 """
-ios1 =('cisco_ios',  '192.168.50.2', 'admin', 'cisco')
-ios2 =('cisco_ios',  '192.168.50.3', 'admin2', 'cisco2')
 
-#bu map, bunun yerine dict olarak yollamaya çalış bu sorun çıkarıyor!!!!!!!!!!!!!
+ios1 =('cisco_ios',  '192.168..', '', '')
+ios2 =('cisco_ios',  '192.168..', '2', '2')
+
+#map yerine dict olarak yolla
 
 t1 = threading.Thread(target=calistir, args=(ios1,))
 t2 = threading.Thread(target=calistir, args=(ios2,))
@@ -34,10 +36,7 @@ t2 = threading.Thread(target=calistir, args=(ios2,))
 t1.start()
 t2.start()
 
-""" Tuple parameters are not supported in Python3, but you can pass it as a variable and unpack it after defining the function.
-
+"""
 def transform_mesh(translate_xyz):
     translate_x, translate_y, translate_z = translate_xyz  
 """
-
-#You're passing a sequence of arguments, for which you use one asterisk. The double-asterisk syntax is for #passing a mapping, for instance to do something like this:
