8bcd8085436b06dc60ab850defe7de2363186325
onurctl <77632313+onurctl@users.noreply.github.com>
2024-02-17 18:54:29 +0300

    Update router-ip-degisim.py

--- pyExamples/router-ip-degisim.py
+++ pyExamples/router-ip-degisim.py
@@ -2,16 +2,16 @@ import netmiko
 from netmiko import ConnectHandler
 ios = {
     'device_type': 'cisco_ios',
-    'ip':   '192.168.50.7',
-    'username': 'admin2',
-    'password': 'cisco2',
+    'ip':   '192.168..',
+    'username': '2',
+    'password': '2',
 }
 net_connect =ConnectHandler(**ios)
 net_connect.enable()
 output =net_connect.send_command('show ip int brief')
 print(output)
 
-config_commands = [ 'int f0/0', 'ip address 192.168.50.10 255.255.255.0', 'no sh']
+config_commands = [ 'int f0/0', 'ip address 192.168.. 255.255.255.0', 'no sh']
 output = net_connect.send_config_set(config_commands)
 print (output)
 
