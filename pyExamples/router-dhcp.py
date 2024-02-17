937ec4796a542f11f8b6a1de099e676cc3c30945
onurctl <77632313+onurctl@users.noreply.github.com>
2024-02-17 18:50:03 +0300

    Update router-dhcp.py

--- pyExamples/router-dhcp.py
+++ pyExamples/router-dhcp.py
@@ -1,17 +1,19 @@
 import netmiko
 from netmiko import ConnectHandler
+
 ios = {
     'device_type': 'cisco_ios',
-    'ip':   '192.168.50.2',
-    'username': 'admin',
-    'password': 'cisco',
+    'ip':   '192.168..',
+    'username': '',
+    'password': '',
 }
+
 net_connect =ConnectHandler(**ios)
 net_connect.enable()
 output =net_connect.send_command('show ip int brief')
 print(output)
 
-config_commands = [ 'conf t', 'service dhcp', 'ip dhcp excluded-address 192.168.100.1 192.168.100.99', 'ip dhcp pool myPool', 'network 192.168.100.0 255.255.255.0', 'lease 2', 'default-router 192.168.100.1' ]
+config_commands = [ 'conf t', 'service dhcp', 'ip dhcp excluded-address 192.168.. 192.168..', 'ip dhcp pool myPool', 'network 192.168.. 255.255..', 'lease 2', 'default-router 192.168..' ]
 
 output = net_connect.send_config_set(config_commands)
 print (output)
@@ -19,4 +21,4 @@ print (output)
 output =net_connect.send_command('show ip int brief')
 print (output)
 
-""" 'domain-name mysite.com', 'lease 0 0 30', 'ip dhcp excluded-address 192.168.100.1 192.168.100.5' """
+""" 'domain-name mysite.com', 'lease 0 0 30', 'ip dhcp excluded-address 192.168.. 192.168..' """
