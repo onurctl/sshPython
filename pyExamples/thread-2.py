39c0ca5307daf06b2eba8f13f9e1433fa1e9ee52
onurctl <77632313+onurctl@users.noreply.github.com>
2024-02-17 19:13:16 +0300

    Update thread-2.py

--- pyExamples/thread-2.py
+++ pyExamples/thread-2.py
@@ -7,34 +7,32 @@ from datetime import datetime
 
 start_time = datetime.now()
 
-#sorunsuz çalışıyor, diğer router a authentication hatası verdi çünkü onun ssh ayarlarını kaydetmemiştik
-
 ios1 = {
    
     'device_type': 'cisco_ios',
-    'ip':   '192.168.50.2',
-    'username': 'admin',
-    'password': 'cisco',
+    'ip':   '192.168..',
+    'username': '',
+    'password': '',
     'port' : 22,    
 }
 
-#  'port' : 22,                                    opsiyonel
-# 'secret': 'C1sco12345',          opsiyonel
+#  'port' : 22,                    opsiyonel
+#  'secret': 'C1sco12345',         opsiyonel
 
 ios2 = {
    
     'device_type': 'cisco_ios',
-    'ip':   '192.168.50.3',
-    'username': 'admin2',
-    'password': 'cisco2',
-    'port' : 22,    #port 22 ekleyince düzeldi kimlik doğrulama hatası, secret ları da ekle gerekirse
+    'ip':   '192.168..',
+    'username': '2',
+    'password': '2',
+    'port' : 22,    
 }
 
 def baglan(device_data):
     net_connect = ConnectHandler(**device_data)
     output = net_connect.send_command('show ip int brief')
     print(net_connect.host)
-    #print("*" * len(net_connect.host)) -- host adının uzunluğu kadar *** koyuyor
+    #print("*" * len(net_connect.host)) 
     print(output)
 
 #parametre probleminin çözümü:
